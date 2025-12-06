b3b2b81be4b9e2e6ef53b2ce3312a25c2e0dd20f
Jay <jay@Jays-MacBook-Air.local>
2025-12-05 21:21:44 -0600

    FIX: Test for db

--- scripts/InstallDependencies.sh
+++ scripts/InstallDependencies.sh
@@ -5,3 +5,4 @@ pip install --upgrade pip
 pip install -r docs/backend_requirements.txt
 pip install google-genai google-api-core python-dotenv pytest groq
 pip install fastapi uvicorn "python-multipart[standard]"
+pip install "psycopg[binary]"
